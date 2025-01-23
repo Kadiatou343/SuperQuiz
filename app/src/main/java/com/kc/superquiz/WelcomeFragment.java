dcecb05a22f7ba264c8774a6e223eba348f20af8
Kadiatou <kimkd08@gmail.com>
2025-01-23 15:58:22 -0500

    view binding in welcome fragment

--- app/src/main/java/com/kc/superquiz/WelcomeFragment.java
+++ app/src/main/java/com/kc/superquiz/WelcomeFragment.java
@@ -2,13 +2,18 @@ package com.kc.superquiz;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.kc.superquiz.databinding.FragmentWelcomeBinding;
+
 public class WelcomeFragment extends Fragment {
+    private FragmentWelcomeBinding binding;
 
     public WelcomeFragment() {
         // Required empty public constructor
@@ -26,7 +31,13 @@ public class WelcomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
+        binding = FragmentWelcomeBinding.inflate(inflater, container, true);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.playButton.setEnabled(false);
     }
 }
